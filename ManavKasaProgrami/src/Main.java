56f0b3acc046e3e8c66ee3dc3121a1de97e04e48
Rabiacagli <rabiaacgl@gmail.com>
2023-12-13 01:07:47 +0300

    Manav Kasa Programı

--- ManavKasaProgrami/src/Main.java
+++ ManavKasaProgrami/src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+//
+//Meyveler ve KG Fiyatları
+//
+//Armut : 2,14 TL
+//Elma : 3,67 TL
+//Domates : 1,11 TL
+//Muz: 0,95 TL
+//Patlıcan : 5,00 TL
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }

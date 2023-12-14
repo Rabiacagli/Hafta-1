db77e166e3f078d52edd29a9bf63f02b45830c2f
Rabiacagli <rabiaacgl@gmail.com>
2023-12-14 23:56:48 +0300

    Düzeltme yapıldı.Uçak bileti Hesaplama Programı(Yaş İndirimlerine Göre)

--- UcakBiletiHesaplamaProgrami/src/Main.java
+++ UcakBiletiHesaplamaProgrami/src/Main.java
@@ -20,27 +20,24 @@ public class Main {
         // Yaş koşullarını ve indirimleri belirliyoruz
         if (age <= 0 || distance <= 0 || (tripType != 1 && tripType != 2)) {
             System.out.println("Hatalı Veri Girdiniz! ");
-        } else {
-
-            //Tek yön olması durumundaki yaş indirimleri
-            if (tripType == 1) {
-
-                if (age < 12) {
-                    price *= 0.50;
-
-                } else if (age >= 12 && age <= 24) {
-                    price *= 0.90;
-
-                } else if (age >= 65) {
-                    price *= 0.70;
-
-                }  if (tripType == 2) {
-                    // Gidiş dönüş seçilmişse %20 indirim
-                    price *= 0.8;
-                    price = price * 2;    // Gidiş dönüş seçilmişse biletler 2 katı
-                }
-                System.out.println("Toplam bilet fiyatı: " + price + " TL");;
+        }  else {
+            if (age < 12) {
+                // 12 yaşından küçükse %50 indirim
+                price *= 0.5;
+            } else if (age >= 12 && age <= 24) {
+                // 12-24 yaş arasındaysa %10 indirim
+                price *= 0.9;
+            } else if (age >= 65) {
+                // 65 yaşından büyükse %30 indirim
+                price *= 0.7;
             }
+
+            if (tripType == 2) {
+                // Gidiş dönüş seçilmişse %20 indirim
+                price *= 0.8;
+                price = price * 2;    // Gidiş dönüş seçilmişse biletler 2 katı
+            }
+            System.out.println("Toplam bilet fiyatı: " + price + " TL");
         }
     }
 }

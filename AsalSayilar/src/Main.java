09cec04b6ad93ad592535588b4f60b430227afc4
Rabiacagli <rabiaacgl@gmail.com>
2023-12-16 22:13:33 +0300

    1 ve 100 arasındaki asal sayılar

---
+++ AsalSayilar/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            int counter = 0;
+            for (int k = 2; k < i; k++) {
+                if (i % k == 0) {
+                    counter++;
+                }
+            }
+            if (counter == 0) {
+                System.out.print(i + " - ");
+
+            }
+        }
+    }
+}

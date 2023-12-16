d00c54cb49aa2393715e17b8c584512f9f29ca37
Rabiacagli <rabiaacgl@gmail.com>
2023-12-16 22:21:03 +0300

    1 ve 100 arasındaki asal sayılar

--- AsalSayilar/src/Main.java
+++ AsalSayilar/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        // 1 ve 100 arasındaki asal sayıları bulmak için koşul oluşturuyoruz
+
+        // counter = sayac, i sayısı k sayısına bölünebiliyorsa asal değildir ve ekrana yazdırmayacak
+
         for (int i = 2; i <= 100; i++) {
             int counter = 0;
             for (int k = 2; k < i; k++) {

fa2362caa5ed20863e71ddd1423146bdd9d50e26
Rabiacagli <rabiaacgl@gmail.com>
2023-12-17 23:35:38 +0300

    Atm Projesi

--- AtmProjesi/src/Main.java
+++ AtmProjesi/src/Main.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
+        //kullanıcıdan veri alabilme için scanner sınıfını kullanıyor ve değişkenleri oluşturuyoruz
 
+        Scanner inp = new Scanner(System.in);
         String userName, password;
         int balance = 1000, price;
         int right = 3;
         int select;
 
-        while (right > 0) {
+        while (right > 0) {                                          // 3 hakkı olma durumu döngüsü
             System.out.print("Lütfen kullanıcı adınızı girin:  ");
             userName = inp.nextLine();
             System.out.print("Lütfen şifrenizi girin: ");
             password = inp.nextLine();
 
 
-            if (userName.equals("patika") && password.equals("dev123")) {
-                System.out.println("Giriş işlemi başarılı.");
+            if (userName.equals("patika") && password.equals("dev123")) {  // kullanıcı adı ve şifre doğruluğu koşulu
+                System.out.println("Giriş işlemi başarılı.");                //hatalı olma durumunda döngü başa dönecek
                 do {
-                    System.out.println("1-Para Yatırma\n2-Para Çekme\n3-Bakiye Sorgulama\n4-Çıkış Yap");
+                    System.out.println("1-Para Yatırma\n2-Para Çekme\n3-Bakiye Sorgulama\n4-Çıkış Yap"); // kullanıcının yapmak istediği işlemler döngüsü
                     System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz: ");
                     select = inp.nextInt();
 
@@ -42,7 +42,6 @@ public class Main {
                         case 3:
                             System.out.println("Güncel Bakiyeniz: " + balance + "TL");
                             break;
-
                     }
                 } while (select != 4);
                 System.out.println("Tekrar görüşmek üzere.");
